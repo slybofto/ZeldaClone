b4d4ecea27891db9bd86b45dbef91cbc435d446b
Connor B <cibofto@gmail.com>
2021-07-19 12:09:57 -0400

    movement updated

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -8,23 +8,26 @@ public class ThirdPersonMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
     public float speed = 6.0f;
-    public float turnSmoothTime = 0.0f;
-    float turnSmoothVelocity;
+    // public float turnSmoothTime = 0.6f;  
+    // float turnSmoothVelocity;
+
     // Update is called once per frame
     void Update()
     {
+        // combines horizontal + vert directional data into a single vec3 object
         float horizontal = Input.GetAxisRaw("Horizontal"); 
         float vertical = Input.GetAxisRaw("Vertical"); 
         Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;
 
+        // checks if w, a, s, d is pressed down (this would increases magnitude from 0 to 1)
         if(direction.magnitude >= 0.1)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime );
-            transform.rotation = Quaternion.Euler(0.0f, angle, 0f);
+            //float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime );
+            transform.rotation = Quaternion.Euler(0.0f, targetAngle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime); 
+            controller.Move(moveDir * speed * Time.deltaTime); 
         }
     }
 }
